531b9315b9dd40ac72ac9a95e565be51a67e89f8
Prati <pratimeshshinde121@gmail.com>
2022-01-12 14:31:33 +0530

    profile pic error

--- components/Header.js
+++ components/Header.js
@@ -47,7 +47,12 @@ function Header() {
             {/* Right */}
             <div className="flex items-center sm:space-x-2 justify-end">
                 {/* Profile Pic */}
-                <Image onClick={signOut} className="rounded-full cursor-pointer" src={session.user.image}/>
+              <Image onClick={signOut} className="rounded-null cursor-pointer" src={session.user.image}
+              width="40"
+              height="40"
+              layout="fixed"
+              />
+
                 <p className="whitespace-nowrap font-semibold pr-3">
                     Pratimesh Shinde
                 </p>
