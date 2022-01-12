adad9a7b2e7c838bd2d1364c929d059cf12d9854
Prati <pratimeshshinde121@gmail.com>
2022-01-12 12:39:44 +0530

    login

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "links.papareact.com",
+      "platform-lookside.fbsbx.com",
+      "firebasestorage.googleapis.com",
+    ],
+  },
+};
